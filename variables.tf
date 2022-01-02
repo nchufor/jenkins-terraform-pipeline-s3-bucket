34892aa52ef2c05ef845db49c1f038de4ed035b5
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-01-01 22:15:35 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,24 +8,11 @@ variable "key_name" {
     default     =  "op" 
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
-}
-
 variable "security_group" { 
     description = "Name of security group" 
     default     = "jenkins-sgroup-dec-2021" 
 }
 
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
-}
 variable "versioning" {
     type        = bool
     description = "(Optional) A state of versioning."
