bdbf8090674c92950965f14007fc2dadeacbbb56
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-01-01 22:35:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
